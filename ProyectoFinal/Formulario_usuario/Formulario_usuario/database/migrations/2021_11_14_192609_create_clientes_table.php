4b0ee93eef3f1838e05aadbd33870e4db87d656d
LAPTOP-BCASVH73\Usuario <Alan Carhuayo>
2021-11-15 17:09:32 -0500

    CARGA DE BACKEND

---
+++ ProyectoFinal/Formulario_usuario/Formulario_usuario/database/migrations/2021_11_14_192609_create_clientes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombres',30);
+            $table->string('correo',30)-> unique();
+            $table->string('telefono',10);
+            $table->string('mensaje',100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
